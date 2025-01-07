0db63649cc0a541c915a61c0f48dd284305cc026
JPac2216 <jakepaccione@gmail.com>
2025-01-07 11:26:11 -0500

    Fixed comparison()

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, flash, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
+import json
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 ALLOWED_EXTENSIONS = {'json'}
 
 def allowed_file(filename):
@@ -29,14 +31,39 @@ def upload_file():
         if followers.filename == '' or following.filename == '':
             flash('No selected file')
             return redirect(request.url)
+        
         if followers and allowed_file(followers.filename) and following and allowed_file(following.filename):
             filename1 = secure_filename(followers.filename)
             filename2 = secure_filename(following.filename)
             followers.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
             following.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
             flash('Files uploaded successfully.')
-            return redirect(request.url)
+
+            result = comparsion()
+            return render_template('index.html')
         else:
             flash('Please upload JSON files only.')
             return redirect(request.url)
     return render_template('index.html')
+        
+        
+def comparsion():
+    followers = set()
+    following = set()
+    with open(os.path.join(UPLOAD_FOLDER, 'followers_1.json'), 'r') as file1:
+        followers_json = json.load(file1)
+        for user in followers_json:
+            if 'string_list_data' in user:
+                data = user['string_list_data']
+                followers.add((data[0]['value']))
+
+    with open(os.path.join(UPLOAD_FOLDER, 'following.json'), 'r') as file2:
+        following_json = json.load(file2)
+        for user in following_json["relationships_following"]:
+            if 'string_list_data' in user:
+                data = user['string_list_data']
+                following.add((data[0]['value']))
+
+    not_followed_back = following - followers  
+
+    return not_followed_back
